2421d2c4a2f7a9e508e76482c938ceb9b6993dfe
JakeKViti <thevitij64@gmail.com>
2021-07-13 00:05:16 -0400

    Create List For Rules.js

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,7 +4,14 @@ export default class Home extends React.Component {
     render() {
         return (
             <div>
-                All Submitions Will Be Under Review
+                <h1>Information About Site:</h1>
+                <p>1: All Submission Will Be Reviewed By App Admin</p>
+                <p>2: Motivation could be a Quote, Saying, or Well Words</p>
+                <p>3: If You Don't Know Who Said The Quote or Don't Want To Say Who Its From, Leave Said By As Anonymous</p>
+                <p>4: If You Don't Know Origin Of Message Leave It As Unknown</p>
+                <p>5: Write How You Want To Be Credited, If You Don't Want To Be Creadited, Leave It As Anonymous</p>
+                <p>6: Please Message Any Concerns Or Questions To @JakeKViti On Twitter</p>
+                <p>7: Rules Will Be Updated As Needed</p>
             </div>
         );
     }
