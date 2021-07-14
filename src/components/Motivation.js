4e9780bb2a770db4f6d64e189dfcb50eeb3bada0
JakeKViti <thevitij64@gmail.com>
2021-07-14 19:29:43 -0400

    Connected fetchQuote to Motivation.js

--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { fetchQuote } from "../actions/fetchQuote";
+import { connect } from 'react-redux'
+import Quote from './Quote'
 
-export default class Motivation extends React.Component {
+ class Motivation extends React.Component {
+
+    componentDidMount = () => {
+        this.props.fetchQuote()
+    }
+  
+    handleLoading = () => {
+        console.log(this.props.entries)
+        if(this.props.entries.length !== 0) {
+            return <Quote entries={this.props.entries} />
+        }
+    }
+
     render() {
         return (
             <div>
                 Here's Some Wise Words
+                {this.handleLoading()}
             </div>
         );
     }
 };
+
+const mapStateToProps = state => {
+    return {
+      entries: state.entries,
+      loading: state.loading
+    }
+  }
+
+
+  export default connect(mapStateToProps, { fetchQuote })(Motivation)
