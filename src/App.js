870de25176d2885b8d1b9385b3fc3bcb58a3e7b0
JakeKViti <thevitij64@gmail.com>
2021-09-03 17:48:12 -0400

    Put Admin in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Motivation from './components/Motivation.js'
 import Form from './components/Form.js'
 import Rules from './components/Rules.js'
 import NavBar from './components/NavBar.js'
+import Admin from './components/Admin.js'
 import {
   BrowserRouter as Router,
   Route
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/Motivation/moti" component={Motivation} />
         <Route exact path="/Motivation/form" component={Form} />
         <Route exact path="/Motivation/about" component={Rules} />
+        <Route exact path="/Motivation/admin" component={Admin} />
       </header>
     </div>
     </Router>
