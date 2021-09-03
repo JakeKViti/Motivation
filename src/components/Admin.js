fcf22769346240d9a8b93c885603cedbea168ba5
JakeKViti <thevitij64@gmail.com>
2021-09-03 17:49:38 -0400

    Put Roadmap in Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -4,7 +4,7 @@ export default class Admin extends React.Component {
     render() {
         return (
             <div>
-                Admin
+                Admin will enter a code, then will receive all not approved Quotes and then Admin will be able to update them
             </div>
         )
     }
