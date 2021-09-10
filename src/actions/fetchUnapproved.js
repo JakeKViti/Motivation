cbe9f08ad2ce4078d54d33455f9665ab40012ae0
JakeKViti <thevitij64@gmail.com>
2021-09-10 00:33:22 -0400

    Update fetchUnapproved.js

---
+++ src/actions/fetchUnapproved.js
@@ -0,0 +1,8 @@
+export const fetchUnapproved = () => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_UNAPPROVED'})
+      fetch('https://ancient-lake-58391.herokuapp.com/api/v1/admin') 
+      .then(response => response.json())
+      .then(entry => dispatch({ type: 'ADD_UNAPPROVED', payload: entry}));
+    }
+  }
